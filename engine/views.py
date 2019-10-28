0f1c693b523fc28c3567d7b190394a6dbbd135d5
Piyus Kumar <piyusgupta01@gmail.com>
2019-10-29 01:03:27 +0530

    added dashboard page

--- engine/views.py
+++ engine/views.py
@@ -9,3 +9,8 @@ def home(request):
     if q:
         search_id = execute_search(q)
     return render(request, 'engine/search.html')
+
+
+def dashboard(request):
+    latest_searches = SearchHistory.objects.all().order_by('-id')[:10]
+    return render(request, 'engine/dashboard.html', {'latest_searches': latest_searches})
--- searchtool/urls.py
+++ searchtool/urls.py
@@ -16,9 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from engine.views import home
+from engine.views import dashboard, home
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home, name='home'),
+    path('dashboard/', dashboard, name='dashboard'),
 ]
